b2459bf2c60f34bd65ca9a2fd8412598152a7569
odrevet <odrevet@gmail.com>
2022-09-21 21:39:20 +0200

    hide dialogbox by default

--- textBox.gd
+++ textBox.gd
@@ -1,11 +1,8 @@
 extends CanvasLayer
 
 func _ready():
-	$MarginContainer/Panel/Label.text = "INIT"
-	#$MarginContainer.hide()
+	$MarginContainer.hide()
 
 func set_text(text):
-	print("Set text to")
-	print(text)
 	$MarginContainer.show()
 	$MarginContainer/Panel/Label.text = text
